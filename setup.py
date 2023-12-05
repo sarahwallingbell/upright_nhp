4fb52c69aea43d62b006ee035d9205b9026bdd70
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2023-12-05 13:59:12 -0800

    update: package files

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-from setuptools import setup, find_packages
-import os
-
-readme_path = os.path.join(os.path.dirname(__file__), "README.md")
-with open(readme_path, "r") as readme_file:
-    readme = readme_file.read()
-
-with open('requirements.txt', 'r') as f:
-    required = f.read().splitlines()
-
-with open("src/__init__.py") as f:
-    for line in f:
-        if line.startswith("__version__"):
-            version = line.split('"')[1]
-
-setup(
-    name = 'primate_uprighting',
-    version = version,
-    description = "Functions for common and not so common morphology operations",
-    long_description=readme,
-    author = "Sarah Walling-Bell",
-    author_email = "sarah.wallingbell@alleninstitute.org",
-    url = 'https://github.com/sarahwallingbell/primate_uprighting',
-    packages = find_packages(),
-    install_requires = required,
-    include_package_data=True,
-    package_data={"morph_utils": ["tests/*"]},
-    setup_requires=['pytest-runner'],
-)
+import setuptools 
+if __name__ == "__main__": 
+   setuptools.setup()
